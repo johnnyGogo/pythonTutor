11935408c26ff99766a3f06992d628bec6e54391
Johnny Liu <johnny.liu@tarokosoftware.com>
2018-11-07 18:00:19 +0800

    enum

--- variables.py
+++ variables.py
@@ -13,6 +13,35 @@ convert type
 - str({})
 """
 import fire
+import enum
+
+class InputType(enum.Enum):
+    TRACK = 'track'
+    ALBUM = 'album'
+    ARTIST = 'atrist'
+    PLAYLIST = 'playlist'
+
+
+class Shape(enum.Enum):
+    SQUARE = 1
+    CIRCLE = 2
+
+
+class Response(enum.Enum):
+    OK = 1
+    BAD = 2
+
+
+class Mac(enum.IntEnum):
+    AIR = 1
+    PRO = 2
+    PRO_PLUS = 4
+
+
+class Android(enum.IntEnum):
+    MARSHMALLOW = 1
+    NOUGAT = 2
+    PRO_PLUS = 4
 
 
 def type_test():
@@ -29,6 +58,26 @@ def type_test():
     print("{{}}:{}".format(type({})))
     print("[]:{}".format(type([])))
     print("():{}".format(type(())))
+
+
+def enum_test():
+    TestEnum = enum.Enum('TestEnum', 'CAT DOG PANDA')
+    print("Get enum name")
+    print("InputType.PLAYLIST.name : {}".format(InputType.PLAYLIST.name))
+    print("Get enum value")
+    print("InputType.PLAYLIST.value : {}".format(InputType.PLAYLIST.value))
+    print("Shape.SQUARE == Response.OK : {}".format(Shape.SQUARE == Response.OK))
+    print("InputType.TRACK == 'track' : {}".format(InputType.TRACK == 'track'))
+    print("Shape.SQUARE == 1 : {}".format(Shape.SQUARE == 1))
+    print("Mac.AIR == 1 : {}".format(Mac.AIR == 1))
+    print("Mac.AIR == Android.MARSHMALLOW : {}".format(Mac.AIR == Android.MARSHMALLOW))
+    print("Mac.AIR | Android.NOUGAT : {}".format(Mac.AIR | Android.NOUGAT))
+
+    print()
+    print(TestEnum)
+    print(TestEnum.CAT.name)
+    print(TestEnum.CAT.value)
+
 
 
 if __name__ == '__main__':
